3e61cf24423914f1329df31b76e5eeaa6af7667a
fatrbaby <fatrbaby@qq.com>
2018-08-06 15:25:04 +0800

    (features)binary to int helper

--- example.php
+++ example.php
@@ -9,11 +9,18 @@ print_r($connection->tables());
 
 $table = $connection->table('hellobase');
 
+
 $table->put('111', ['hb:id' => 'id:111', 'hb:name' => 'name:111']);
 
 foreach ($table->scan() as $row => $columns) {
     var_dump($row, $columns);
 }
+
+//$table->increment('222', 'hb:counter', 5);
+
+$raw = $table->row('222');
+
+echo \HelloBase\Supports\Integer::binToInt($raw['hb:counter']), PHP_EOL;
 
 $connection->close();
 
